22c7d9cbbea6bb8a67f0884d4b623305317104e4
Jung's Study <JungsArk@gmail.com>
2025-11-20 21:26:37 +0900

    [feature/lecture/create-entity] : Lecture 엔티티 생성

---
+++ src/main/java/com/edutrack/domain/lecture/entity/Lecture.java
@@ -0,0 +1,56 @@
+package com.edutrack.domain.lecture.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import org.apache.catalina.User;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
+@Table(name = "lecture")
+public class Lecture {
+
+  public enum DayOfWeek {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private BigInteger id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "academy_id", referencedColumnName = "id",  nullable = false)
+  private Academy academy;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "teacher_id", referencedColumnName = "id", nullable = false)
+  private User teacher;
+
+  @Column(nullable = false)
+  private String title;
+
+  @Column(nullable = false)
+  private String description;
+
+  @Enumerated(EnumType.STRING) @Column(nullable = false)
+  private DayOfWeek dayOfWeek;
+
+  @Column(nullable = false)
+  private LocalDateTime startDate;
+
+  @Column(nullable = false)
+  private LocalDateTime endDate;
+
+  @Column(nullable = false)
+  private Timestamp created_at;
+}
