d847fa63774e7392c80cbbdc71378412b3040956
jyong yong hwa <kobcom12@naver.com>
2025-11-23 05:52:13 +0900

    feat(user): 사용자 정보 조회 엔드포인트 추가

--- src/main/java/com/edutrack/domain/user/controller/UserController.java
+++ src/main/java/com/edutrack/domain/user/controller/UserController.java
@@ -1,24 +1,36 @@
 package com.edutrack.domain.user.controller;
 
+import com.edutrack.domain.user.dto.MyInfoResponse;
 import com.edutrack.domain.user.dto.SignInRequest;
 import com.edutrack.domain.user.dto.SignInResponse;
 import com.edutrack.domain.user.service.UserAuthService;
+import com.edutrack.global.security.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserAuthService userAuthService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @PostMapping("/signin")
     public ResponseEntity<SignInResponse> signIn(@RequestBody SignInRequest request) {
         SignInResponse response = userAuthService.signIn(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MyInfoResponse> me(
+            @RequestHeader("Authorization") String authHeader
+    ) {
+
+        String token = authHeader.replace("Bearer ", "");
+        Long userId = jwtTokenProvider.getUserIdFromToken(token);
+
+        MyInfoResponse response = userAuthService.getMyInfo(userId);
         return ResponseEntity.ok(response);
     }
 }
