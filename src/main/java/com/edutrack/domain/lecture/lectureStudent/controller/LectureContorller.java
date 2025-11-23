8736ad898b6937dfcf25c2298246c9985299b5bf
Jung's Study <JungsArk@gmail.com>
2025-11-24 01:30:39 +0900

    [feature/lecture/get-lectures] : lecture 의 controller 생성

---
+++ src/main/java/com/edutrack/domain/lecture/lectureStudent/controller/LectureContorller.java
@@ -0,0 +1,26 @@
+package com.edutrack.domain.lecture.lectureStudent.controller;
+
+import com.edutrack.domain.lecture.dto.LectureForTeacherResponseDto;
+import com.edutrack.domain.lecture.service.LectureService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/lectures")
+@RequiredArgsConstructor
+public class LectureContorller {
+
+  private final LectureService lectureService;
+
+  @GetMapping()
+  public ResponseEntity<List<LectureForTeacherResponseDto>> getLecturesByTeacherId(@RequestParam Long teacherId) {
+    List<LectureForTeacherResponseDto> lectures = lectureService.getLecturesByTeacherId(teacherId);
+    return ResponseEntity.ok(lectures);
+  }
+
+}
