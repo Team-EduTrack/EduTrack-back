eb68e39918ab185ad82774ffc54a7d507e04b4f2
Jung's Study <JungsArk@gmail.com>
2025-11-24 23:44:00 +0900

    [feature/lecture/get-lectures-v2] : 엔티티에 올바른 User Import

--- src/main/java/com/edutrack/domain/lecture/entity/LectureStudent.java
+++ src/main/java/com/edutrack/domain/lecture/entity/LectureStudent.java
@@ -1,7 +1,8 @@
 package com.edutrack.domain.lecture.entity;
 
-import com.edutrack.domain.user.User;
+import com.edutrack.domain.user.entity.User;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -32,6 +33,7 @@ public class LectureStudent {
     @Column(name = "lecture_expire_date")
     private LocalDateTime expireDate;
 
+    @Builder
     public LectureStudent(Lecture lecture, User student) {
         this.id = new LectureStudentId(lecture.getId(), student.getId());
         this.lecture = lecture;
