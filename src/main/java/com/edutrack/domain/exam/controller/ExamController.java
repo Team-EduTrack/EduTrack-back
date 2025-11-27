f510f0506abb63fd4cc480eadea3d1d2d4664962
Hyoeun Jung <jungsand1325@gmail.com>
2025-11-27 20:58:11 +0900

    feat : 시험생성, 문제등록 controller 구현

---
+++ src/main/java/com/edutrack/domain/exam/controller/ExamController.java
@@ -0,0 +1,57 @@
+package com.edutrack.domain.exam.controller;
+
+import com.edutrack.domain.exam.dto.ExamCreationRequest;
+import com.edutrack.domain.exam.dto.ExamCreationResponse;
+import com.edutrack.domain.exam.dto.QuestionRegistrationRequest;
+import com.edutrack.domain.exam.service.ExamService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/lectures/{lectureId}/exams")
+public class ExamController {
+    private final ExamService examService;
+
+    //시험생성
+    @PostMapping
+    @PreAuthorize("hasRole('PRINCIPAL', 'TEACHER')")
+    public ResponseEntity<ExamCreationResponse> createExam(
+            @PathVariable Long lectureId,
+            @Valid @RequestBody ExamCreationRequest request,
+            Authentication authentication){
+        Long principalUserId = (Long) authentication.getPrincipal();
+        request.setLectureId(lectureId);
+
+        Long examId = examService.createExam(principalUserId, request);
+
+        ExamCreationResponse response = ExamCreationResponse.builder()
+                .examId(examId)
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    //문제등록
+
+    @PostMapping("/{examId}/mcq")
+    @PreAuthorize("hasRole('PRINCIPAL', 'TEACHER')")
+    public ResponseEntity<List<Long>> registerQuestions(
+            @PathVariable Long examId,
+            @Valid @RequestBody List<QuestionRegistrationRequest> requests){
+
+        List<Long> questionIds = examService.registerQuestions(examId, requests);
+
+        return ResponseEntity.status(HttpStatus.OK).body(questionIds);
+    }
+
+
+
+
+}
