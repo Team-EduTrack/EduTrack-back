516e535fdf1bbd40ce37b2c7b719d979d39db8bd
Hyoeun Jung <jungsand1325@gmail.com>
2025-12-05 20:55:06 +0900

    feat: 응답 dto 정의

---
+++ src/main/java/com/edutrack/domain/statistics/ExamDistributionResponse.java
@@ -0,0 +1,18 @@
+package com.edutrack.domain.statistics;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class ExamDistributionResponse {
+    //점수구간
+    private final List<String> ranges;
+
+    private final List<Integer> counts;
+
+    private final int totalSubmissions;
+
+}
